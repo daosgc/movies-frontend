1946e7d24044f9165d789fccbd2086089d76ce13
Darwin Gualotuña <daosgc@gmail.com>
2020-07-05 14:42:30 -0500

    Create movie component

---
+++ src/components/movie/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Movie.scss';
+
+const DEFAULT_PLACEHOLDER_IMAGE =
+  "https://cinemaone.net/images/movie_placeholder.png";
+
+const MovieComponent = ({ movie }) => {
+  const poster =
+    movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : DEFAULT_PLACEHOLDER_IMAGE;
+  return (
+    <div className="movie-card">
+      <img className="poster" src={poster} />
+      <div className="footer">
+        {movie.original_title}
+      </div>
+    </div>
+  );
+};
+
+export default MovieComponent;
